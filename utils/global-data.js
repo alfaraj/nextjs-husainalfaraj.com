3e73bded3ee81c3848e235715c730b29c4ff6f2b
Husain Al-Faraj <husainalfaraj@gmail.com>
2022-07-14 16:33:36 +0300

    fixes

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'HUSAIN ALFARAJ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : "I'm Husain";
+    : "Hello, I'm Husain!";
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
